78381fc269a09b9242a1dbfacf9aae4fe726ed42
Lasse27 <llhillen1403@gmail.com>
2025-02-25 19:10:10 +0100

    Add: Icon and Style added and unnecessary # removed

--- leaflens/FLASK/LeafLensUI.py
+++ leaflens/FLASK/LeafLensUI.py
@@ -2,14 +2,8 @@ from flask import Flask, render_template
 from flaskwebgui import FlaskUI  # type: ignore
 
 
-#
-#
-
 # Erstellen der Flask-App
 app = Flask(__name__)
-
-#
-#
 
 
 @app.route("/")
@@ -18,19 +12,12 @@ def index():
     return render_template("index.html")
 
 
-#
-#
-
-
 def start() -> None:
     """Starts the applications ui."""
     FlaskUI(
         app=app, port=80, fullscreen=False, width=1280, height=800, server="flask"
     ).run()
 
-
-#
-#
 
 if __name__ == "__main__":
     start()
