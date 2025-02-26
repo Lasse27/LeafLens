6a2ad45d3a469451ffe72531a360ce3188bb3ab4
Lasse27 <llhillen1403@gmail.com>
2025-02-26 19:18:08 +0100

    Change: Changed test case to support linux(?)

--- tests/OpenCV_test.py
+++ tests/OpenCV_test.py
@@ -1,12 +1,13 @@
 import cv2 as cv
 from unittest import TestCase
+import os
 
 
 class UUT(TestCase):
     # Test OpenCV.imread on .ico files
     def test_imread_ico(self):
         try:
-            path: str = r"tests\ressources\icon.ico"
+            path: str = os.path.join(os.path.dirname(__file__), "ressources", "icon.ico")
             img = cv.imread(path)
             if img is None:
                 raise Exception("Image couldn't be loaded")
